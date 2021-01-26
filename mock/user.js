720287f1877f4c87df7f21aa9b8686d044318d9f
gracesu04133021 <suwenjing8@jd.com>
2021-01-26 23:35:36 +0800

    test

--- mock/user.js
+++ mock/user.js
@@ -13,7 +13,7 @@ const users = {
     roles: ['admin'],
     introduction: 'I am a super administrator',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-    name: 'suwenjing'
+    name: 'suwenjing11111'
   },
   'editor-token': {
     roles: ['editor'],
